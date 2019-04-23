7af416f26a5a4d6b104a19bb2cd4566f249e81c9
chandan169 <chandankumar1694@gmail.com>
2019-04-23 18:30:45 +0530

    adding all files to repo

---
+++ DempMvcApplication/DempMvcApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DempMvcApplication.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DempMvcApplication.Controllers
+{
+    [Table("tblEmployee")]
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            return View();
+        }        
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            //Employee employee= employeeContext.Employees.Single(e=>e.EmployeeId==id);
+            List<Employee> employee=employeeContext.Employees.ToList();
+            return View(employee);
+        }
+
+    }
+}
---
+++ DempMvcApplication/DempMvcApplication/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DempMvcApplication.Models
+{
+    public class Employee
+    {
+        public Int64 EmployeeId { get; set; }
+        public string Name { get; set; }
+        public long DepartmentId { get; set; }
+        public string City { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
